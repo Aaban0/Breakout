9451b25b0308e7af4e3a5725e6e95b99a4b131b1
Aaban <aabanafzal05@gmail.com>
2025-04-16 21:23:17 +0100

    Character 2 Working

--- Breakout/Assets/Scripts/Character2.cs
+++ Breakout/Assets/Scripts/Character2.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character2 : MonoBehaviour
 {
+    public PlayerHealth health;
+
+    public Image powerBar;
+    public TextMeshProUGUI e;
+
+    public float powerCurrent = 0;
+    public float powerMax = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        powerCurrent = 0;
+        health = FindObjectOfType<PlayerHealth>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        powerCurrent += Time.deltaTime * 10f;
+
+        powerBar.fillAmount = powerCurrent / powerMax;
+
+        if (powerCurrent >= powerMax)
+        {
+            e.enabled = true;
+        }
+        else
+        {
+            e.enabled = false;
+        }
+
+        if (powerCurrent >= powerMax && Input.GetKeyDown(KeyCode.E))
+        {
+            health.Heal(30);
+            powerCurrent = 0;
+        }
     }
 }
