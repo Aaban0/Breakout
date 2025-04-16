3cc07e4703e52a2d9b29019773302df3cdce7890
Aaban0 <aabanafzal05@gmail.com>
2025-04-16 17:00:34 +0100

    Character 1 Power Up Added

--- Breakout/Assets/Scripts/Character1.cs
+++ Breakout/Assets/Scripts/Character1.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character1 : MonoBehaviour
 {
+    public EnemyHealth enemyHealth;
+
+    public Image powerBar;
+    public float powerCurrent = 0;
+    public float powerMax = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        powerCurrent = 0;
+        enemyHealth = FindObjectOfType<EnemyHealth>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        powerCurrent += Time.deltaTime * 10f;
+
+        powerBar.fillAmount = powerCurrent / powerMax;
+
+        if (powerCurrent >= powerMax && Input.GetKeyDown(KeyCode.E))
+        {
+            enemyHealth.Damage(20);
+            powerCurrent = 0;
+        }
     }
 }
