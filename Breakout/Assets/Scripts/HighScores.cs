f938a30c595414623503b522e9d5dd8aedd8fd3b
Aaban0 <aabanafzal05@gmail.com>
2025-04-16 15:37:14 +0100

    Working on High Scores

--- Breakout/Assets/Scripts/HighScores.cs
+++ Breakout/Assets/Scripts/HighScores.cs
@@ -6,29 +6,45 @@ using TMPro;
 public class HighScores : MonoBehaviour
 {
     private int highScore1;
+    private int highScore2;
+    private int highScore3;
+    private int highScore4;
+    private int highScore5;
+
     public TextMeshProUGUI score1;
-    /*public TextMeshProUGUI score2;
+    public TextMeshProUGUI score2;
+
     public TextMeshProUGUI score3;
     public TextMeshProUGUI score4;
-    public TextMeshProUGUI score5;*/
+    public TextMeshProUGUI score5;
 
     private void Update()
     {
         CheckHighScore();
     }
 
+
     private void CheckHighScore()
     {
         if (PlayerPrefs.GetInt("score") > PlayerPrefs.GetInt("HighScore", 0))
         {
             PlayerPrefs.SetInt("HighScore", PlayerPrefs.GetInt("score"));
-
-            //highScore1 = PlayerPrefs.GetInt("HighScore");
-            score1.text = $" Score: {PlayerPrefs.GetInt("HighScore"/*, highScore1*/)}";
+            score1.text = $" Score 1: {PlayerPrefs.GetInt("HighScore")}";
         }
         else
         {
-            score1.text = $" Score: {PlayerPrefs.GetInt("HighScore")}";
+            score1.text = $" Score 1: {PlayerPrefs.GetInt("HighScore")}";
+        }
+
+
+        if (PlayerPrefs.GetInt("score") < PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore2", PlayerPrefs.GetInt("score"));
+            score2.text = $" Score 2: {PlayerPrefs.GetInt("HighScore2")}";
+        }
+        else
+        {
+            score1.text = $" Score 2: {PlayerPrefs.GetInt("HighScore")}";
         }
     }
 }
