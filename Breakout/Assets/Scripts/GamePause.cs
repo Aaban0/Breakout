2a888990142ab98f0739e1a90206b04851d8abf6
Aaban0 <aabanafzal05@gmail.com>
2025-04-17 11:35:27 +0100

    Pause Menu Added

---
+++ Breakout/Assets/Scripts/GamePause.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePause : MonoBehaviour
+{
+    public GameObject GamePauseObject;
+    public GameObject YouWin;
+    public GameObject GameOver;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && GameOver.activeSelf == false && YouWin.activeSelf == false)
+        {
+            GamePauseObject.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void Continue()
+    {
+        GamePauseObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void MainMenu()
+    {
+        GamePauseObject.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
